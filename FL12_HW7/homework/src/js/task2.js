eb04731a866a9b505b946285cde4edc011f463fa
Stas <stanislav.shendryk@gmail.com>
2019-12-30 01:34:54 +0200

    problem in total prize

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,29 @@
-// Your code goes here
+let startGame = confirm('Do you want to play a game?');
+let attempt = 0;
+let money = 0;
+let maxAttempt = 3;
+let maxThinkNumber = 8;
+let maxWinMoney = 100;
+let thinkOfNumber;
+if (startGame) {
+    console.log('start game');
+    let thinkOfNumber = Math.floor(Math.random() * (maxThinkNumber - 0 + 1));
+    console.log(thinkOfNumber);
+    // let money;
+    for(let i=0; attempt < maxAttempt; i++) {
+        attempt++
+        let inputNumber = prompt(`Choose a roulette pocket number from 0 to ${maxThinkNumber}
+Attempt ${attempt}
+Total prize: ${money}
+Possible prize on current attempt: ${maxWinMoney}
+${thinkOfNumber}`);  
+        
+        if (thinkOfNumber === inputNumber) {
+           money = 100;
+        }
+    }
+    // if(inputNumber)
+
+} else {
+    alert('You did not become a billionaire, but can.');
+}
