29c015ac59fcfbf3b9ef271a9b9844946fc99ab1
Stas <stanislav.shendryk@gmail.com>
2019-12-30 23:59:20 +0200

    done

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,29 +1,71 @@
-let startGame = confirm('Do you want to play a game?');
-let attempt = 0;
-let money = 0;
-let maxAttempt = 3;
-let maxThinkNumber = 8;
-let maxWinMoney = 100;
+let gameCont = true;
+let gameNew = false;
+let attempts = 3;
+let maxPrize = 100;
+let totalPrize = 0;
+let maxNumber = 4;
+let victory = false;
+let answer;
 let thinkOfNumber;
-if (startGame) {
-    console.log('start game');
-    let thinkOfNumber = Math.floor(Math.random() * (maxThinkNumber - 0 + 1));
-    console.log(thinkOfNumber);
-    // let money;
-    for(let i=0; attempt < maxAttempt; i++) {
-        attempt++
-        let inputNumber = prompt(`Choose a roulette pocket number from 0 to ${maxThinkNumber}
-Attempt ${attempt}
-Total prize: ${money}
-Possible prize on current attempt: ${maxWinMoney}
-${thinkOfNumber}`);  
-        
-        if (thinkOfNumber === inputNumber) {
-           money = 100;
+
+
+gameCont = confirm('Do you want to play a game?');
+
+if(!gameCont){
+    alert('You did not become a billionaire, but can');
+} else {
+
+    while(gameCont){
+
+        if (gameNew){
+            gameNew = false;
+            totalPrize = 0;
+            attempts = 3;
+            maxNumber = 8;
+            maxPrize = 100;
+            
+        } else {
+            maxNumber += maxNumber;
+            attempts = 3;
+            if (window.onload) {
+                maxPrize; 
+            } else {
+                maxPrize += maxPrize;
+            }
+            
+        }
+
+        thinkOfNumber = Math.floor(Math.random() * (maxNumber - 0 + 1)); 
+        victory=false;
+
+        for (let i=3; i>=1; i--) {
+            answer = +prompt(`Choose a roulette pocket number from 0 to ${maxNumber}.
+Attempts left: ${i}
+Total prize: ${totalPrize} $
+Possible prize on current attempt: ${maxPrize} $ ${thinkOfNumber}`);
+
+            if (answer === thinkOfNumber){
+                totalPrize += maxPrize;
+                victory = true;
+
+                if (!confirm(`Congratulation, you won!   Your prize is: ${totalPrize} $ Do you want to continue?`)){
+                    alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+                    gameCont = false;
+                    gameNew = confirm('Want to play from the start?');
+
+                    if (gameNew){
+                        gameCont = true;
+                    }
+                }
+                break;
+            }
+            maxPrize = maxPrize/2;
+        }
+        if (!victory){
+            alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+            gameNew = confirm('Want to play from the start?');
+            gameCont = gameNew;
+
         }
     }
-    // if(inputNumber)
-
-} else {
-    alert('You did not become a billionaire, but can.');
 }
