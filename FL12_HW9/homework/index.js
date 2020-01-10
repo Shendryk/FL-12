7e6d797d1fca8fb53384a80f8b7cfe68272086c4
Shendryk <stanislav.shendryk@gmail.com>
2020-01-10 18:05:58 +0200

    done task1 convert()

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+function convert(){
+  let arr = [];
+  for(let i=0;i<arguments.length;i++){
+    typeof(arguments[i]) === 'number' ? arr.push(String(arguments[i])):arr.push(Number(arguments[i]));
+  }
+ return arr; 
+}
