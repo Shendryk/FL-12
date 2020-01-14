94f6dca2bbdbbf1c102cf5147e91773ad704410a
Shendryk <stanislav.shendryk@gmail.com>
2020-01-14 18:28:51 +0200

    done task 2-3

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,7 +1,25 @@
+// Task 1
+
 function convert(){
-  let arr = [];
-  for(let i=0;i<arguments.length;i++){
-    typeof(arguments[i]) === 'number' ? arr.push(String(arguments[i])):arr.push(Number(arguments[i]));
-  }
+	let arr = [];
+	for(let i=0; i < arguments.length; i++){
+		typeof arguments[i] === 'number' ? arr.push(String(arguments[i])) : arr.push(Number(arguments[i]));
+	}
  return arr; 
+}
+
+// Task 2
+
+function executeForEach(arr, func) {
+	let resultArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		typeof arr[i] === 'number' ? resultArr.push(func(arr[i])) : resultArr.push(func(parseInt(arr[i]))) ;
+		}
+		return resultArr;
+}
+
+// Task 3
+
+function mapArray(arr, func) {
+		return executeForEach(arr, func);
 }
