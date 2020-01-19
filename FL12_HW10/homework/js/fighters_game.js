4082bd74c519fcd624b4aa2ee1b2faf7f115b360
Stas <stanislav.shendryk@gmail.com>
2020-01-20 00:33:15 +0200

    work on attack

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,1 +1,34 @@
-/* Your code goes here */
+function Fighter(obj){
+    this._obj = obj;
+
+    Fighter.prototype.getName = function () {
+        return this._obj.name;
+    };
+      
+    Fighter.prototype.getDamage = function () {
+        return this._obj.damage;
+    };
+      
+    Fighter.prototype.getStrength = function () {
+        return this._obj.strength;
+    };
+      
+    Fighter.prototype.getAgility = function () {
+        return this._obj.agility;
+    };
+      
+    Fighter.prototype.getHealth = function () {
+        return this._obj.hp;
+    };
+
+    Fighter.prototype.getHealth = function () {
+        return this._obj.hp;
+    };
+
+    // Fighter.prototype.attack = function () {
+
+        
+    // };
+}
+
+const myFighter = new Fighter({name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25});
