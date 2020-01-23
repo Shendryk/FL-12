b073d6380f46a219f7aeeb70ab28b2a39aadab5c
Shendryk <stanislav.shendryk@gmail.com>
2020-01-23 18:55:45 +0200

    save

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -1,40 +1,58 @@
 const structure = [
-    {
-      'folder': true,
-      'title': 'Films',
-      'children': [
-        {
-          'title': 'Iron Man.avi'
-        },
-        {
-          'folder': true,
-          'title': 'Fantasy',
-          'children': [
-            {
-              'title': 'The Lord of the Rings.avi'
-            },
-            {
-              'folder': true,
-              'title': 'New folder 1',
-              'children': false
-            }
-          ]
-        }
-      ]
-    },
-    {
-      'folder': true,
-      'title': 'Documents',
-      'children': [
-        {
-          'folder': true,
-          'title': 'EPAM Homework answers',
-          'children': null
-        }
-      ]
-    }
+  {
+    'folder': true,
+    'title': 'Films',
+    'children': [
+      {
+        'title': 'Iron Man.avi'
+      },
+      {
+        'folder': true,
+        'title': 'Fantasy',
+        'children': [
+          {
+            'title': 'The Lord of the Rings.avi'
+          },
+          {
+            'folder': true,
+            'title': 'New folder 1',
+            'children': false
+          }
+        ]
+      }
+    ]
+  },
+  {
+    'folder': true,
+    'title': 'Documents',
+    'children': [
+      {
+        'folder': true,
+        'title': 'EPAM Homework answers',
+        'children': null
+      }
+    ]
+  }
 ];
 
 const rootNode = document.getElementById('root');
 
 // Todo: your code goes here
+for (let i = 0; i < structure.length; i += 1) {
+  let obj = structure[i];
+  //   console.log(obj.folder);
+  // if (obj.folder = true){
+
+  //   console.log(`${obj.folder}`);
+  // }
+  for (let key in obj) {
+    let value = obj[key];
+    console.log(obj)
+    console.log(`key: ${key}, value: ${value}`);
+    console.log(`${key}: ${value}`);
+    console.log(obj.folder)
+    //     if(obj.folder)
+
+    // }
+  }
+}
