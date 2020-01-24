49920b515944c0e480ff9cfaf6a17d75f376cddd
Shendryk <stanislav.shendryk@gmail.com>
2020-01-24 18:02:02 +0200

    save2

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -40,19 +40,31 @@ const rootNode = document.getElementById('root');
 // Todo: your code goes here
 for (let i = 0; i < structure.length; i += 1) {
   let obj = structure[i];
-  //   console.log(obj.folder);
-  // if (obj.folder = true){
+	//   console.log(obj.folder);
+	
+  if (obj.folder === true ){
+		// console.log(1)
+		console.log(`${obj.folder}`);
+		// let ul = document.createElement('UL');
+		let li = document.createElement('LI');
+		li.innerHTML = obj.title;
+		rootNode.appendChild(li);
 
-  //   console.log(`${obj.folder}`);
-  // }
-  for (let key in obj) {
-    let value = obj[key];
-    console.log(obj)
-    console.log(`key: ${key}, value: ${value}`);
-    console.log(`${key}: ${value}`);
-    console.log(obj.folder)
+		if (obj.children !== null ) {
+			console.log(5);
+		}
+		// .appendChild(li);
+
+  }
+  // for (let key in obj) {
+  //   let value = obj[key];
+    // console.log(obj)
+		// console.log(`key: ${key}, value: ${value}`);
+		// if ()
+    // console.log(`${key}: ${value}`);
+    // console.log(obj.folder)
     //     if(obj.folder)
 
     // }
-  }
+  // }
 }
